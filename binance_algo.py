b0ee5ab6448bc9fe7c5fd46298fb9235f14b09cf
Tomer <tomer.traub@gmail.com>
2023-06-04 12:06:18 +0200

    remove non active symbols

--- binance_algo.py
+++ binance_algo.py
@@ -61,6 +61,7 @@ class crypto_bot:
     self._strategy = payload["strategy"]
     if self._strategy == 'DIY':
         # print(f"Strategy: {self._strategy}")
+        self._interval = payload["interval"]
         self._time_scale = payload["interval"] + "m"
         self._indicators = payload["indicators"]
         self._buy_rule = payload["buyRule"]
@@ -212,29 +213,18 @@ class crypto_bot:
           self.pairs = sorted(stockList)[0:100]
 
   def remove_non_active_tokens(self):
+      # or (not isinstance(self.market_prices[token].index[-1], datetime)) \
       try:
           #sync self.pairs with quotes
           self.pairs = sorted(list(self.market_prices.keys()))
           # Remove empty quotes
+          max_dateTime = max([self.market_prices[i].index[-1] for i in self.pairs if isinstance(self.market_prices[i].index[-1], datetime)])
           for token in self.pairs:
-                try:
-                    if (self.market_prices[token].index[-1]):
-                      continue
-                except BaseException as e:
-                    print(f"Removing token {token} : {e}")
+                if (len(self.market_prices[token])) \
+                        or (self.market_prices[token].index[-1] != max_dateTime) \
+                        or (((datetime.strptime(self._now(), '%Y-%m-%d %H:%M:%S') - self.market_prices[token].index[-1]).seconds) > int(self._interval) * 60):
                     self.market_prices.pop(token)
                     self.pairs.remove(token)
-                    continue
-          # remove tickers which are not traded recently
-          max_dateTime = max([self.market_prices[i].index[-1] for i in self.pairs if isinstance(self.market_prices[i].index[-1], datetime) ])
-          for token in self.pairs:
-              if (self.market_prices[token].index[-1] != max_dateTime) \
-                or (not isinstance(self.market_prices[token].index[-1], datetime))\
-                 or (((datetime.strptime(self._now(), '%Y-%m-%d %H:%M:%S') - self.market_prices[token].index[-1]).seconds) > 36000):
-                  self.printf(f"{self._now()}: Token {token} Not valid last trading date: {self.market_prices[token].index[-1]}. Token will be removed.", {self._now()}, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA")
-                  # print("removing symbol not recently traded:",token)
-                  self.market_prices.pop(token)
-                  self.pairs.remove(token)
       except BaseException as e:
           self.printf(f"{self._now()}: Error in remove not active tokens: {e}", {self._now()}, "NA", "NA", "NA", "NA", "NA","NA", "NA", "NA", "NA")
       return None
